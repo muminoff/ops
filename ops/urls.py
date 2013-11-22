4194745219514c9128d5534ff09aa92addb6578e
muminoff <smuminov@gmail.com>
2013-11-23 00:40:10 +0900

    Frontend design first try

--- ops/urls.py
+++ ops/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from ops.views import *
+
 urlpatterns = patterns('',
+    url(r'^', home_page, name="home_page"),
     url(r'^admin/', include(admin.site.urls)),
 )
